474c83306e31c248fe00671ae3aa2fa3d1f33cd0
Rahul <165404715+Rahulkch@users.noreply.github.com>
2024-06-26 22:04:25 +0530

    Update db.js
    database updates with seek time

--- components/db.js
+++ components/db.js
@@ -2,7 +2,12 @@ const mongoose=require("mongoose");
 require("dotenv").config();
 
 const xyz_database=() =>{
-    mongoose.connect(process.env.DATABASE_URL)
+  mongoose.connect(process.env.DATABASE_URL,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        socketTimeoutMS: 45000,
+        connectTimeoutMS: 30000
+    })
     .then( () => console.log("db is connection succesfully and this is test mongoose"))
     .catch((e) => {
         console.log("issue in connecting data bse ",e);
