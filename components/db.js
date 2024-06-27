2d73c0fc6f7deacb5db0ff00eb24c981e89c7074
Rahul <165404715+Rahulkch@users.noreply.github.com>
2024-06-27 14:40:40 +0530

    Update db.js
    db remove extra

--- components/db.js
+++ components/db.js
@@ -2,12 +2,7 @@ const mongoose=require("mongoose");
 require("dotenv").config();
 
 const xyz_database=() =>{
-  mongoose.connect(process.env.DATABASE_URL,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-         socketTimeoutMS: 2147483647, // Maximum value for a 32-bit signed integer
-    connectTimeoutMS: 2147483647
-    })
+  mongoose.connect(process.env.DATABASE_URL)
     .then( () => console.log("db is connection succesfully and this is test mongoose"))
     .catch((e) => {
         console.log("issue in connecting data bse ",e);
